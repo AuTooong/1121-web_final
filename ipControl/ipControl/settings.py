7a0f49adc366f2f03591943387b22671ba008986
idkidkidkidkidkidkidkidk <57472359+idkidkidkidkidkidkidkidk@users.noreply.github.com>
2024-01-04 23:39:11 +0800

    Added template in ipControl/template

--- ipControl/ipControl/settings.py
+++ ipControl/ipControl/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ipControl.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "ipControl" / "templates" ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
