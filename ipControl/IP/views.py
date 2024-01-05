850b2fefe077936a1f70d17acb2013a7056a3491
idkidkidkidkidkidkidkidk <57472359+idkidkidkidkidkidkidkidk@users.noreply.github.com>
2024-01-05 20:40:19 +0800

    implemented ip search

--- ipControl/IP/views.py
+++ ipControl/IP/views.py
@@ -4,9 +4,14 @@ from django.template import loader
 from django.http import HttpResponse
 
 def overview(request):
-  all_ip = IP.objects.all()
+  ip_query = request.GET.get('ip_query')
+  if ip_query:
+    results = IP.objects.filter(ip__icontains=ip_query)
+  else:
+    results = IP.objects.all()
   template = loader.get_template('overview.html')
   context = {
-    'all_ip': all_ip,
+    'ip_query': ip_query,
+    'results': results
   }
   return HttpResponse(template.render(context, request))
