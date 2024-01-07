690a297386a20b152d5eb5018880631e5327da2a
idkidkidkidkidkidkidkidk <57472359+idkidkidkidkidkidkidkidk@users.noreply.github.com>
2024-01-07 22:58:49 +0800

    added ip filter tabs

--- ipControl/IP/views.py
+++ ipControl/IP/views.py
@@ -2,11 +2,18 @@ from django.shortcuts import render
 from .models import IP, Owner
 from django.template import loader
 from django.http import HttpResponse
+from django.db.models import Q
 
 def overview(request):
   ip_query = request.GET.get('ip_query')
+
+  current_filter = request.GET.get('current_filter')
   if ip_query:
     results = IP.objects.filter(ip__icontains=ip_query)
+    # if current_filter == "iot":
+    #   results = results.filter(service="NAS")
+    # if current_filter == "nas":
+    #   results = results.filter(Q(service="印表機") | Q(name="攝影機"))
   else:
     results = IP.objects.all()
   owner = Owner.objects.all()
